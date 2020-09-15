f9a821acf059edd1ce17cd6b90176a385ee20f05
zzy <390021310@qq.com>
2020-09-15 22:39:25 +0800

    添加header 组件

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Layout, Row, Col, Button, Space} from 'antd'
-import {FolderAddOutlined,PlusCircleOutlined} from '@ant-design/icons';
+import {FolderAddOutlined,PlusCircleOutlined, HeartOutlined, SettingOutlined, ReloadOutlined, UserAddOutlined, DownOutlined, AppstoreOutlined} from '@ant-design/icons';
 
 const { Header } = Layout;
 
@@ -8,20 +8,38 @@ const HeaderComponent = ()=>{
 
     return(
         <Header style={{height:50,background:'#CCCCCC'}}>
-            <Row justify='space-around' align='middle' style={{height:50}}>
+            <Row justify='space-between' align='middle' style={{height:50}}>
                 <Col span={8}>
-                    <Row align="middle">
+                    <Row align="middle" justify='start'>
                         <Space style={{height:50}}>
-                            <Button icon={<PlusCircleOutlined />}>New</Button>
+                            <Button icon={<PlusCircleOutlined />} type='primary'>New</Button>
                             <Button >Import</Button>
                             <Button >Runner</Button>
                             <Button icon={<FolderAddOutlined />}></Button>
                     </Space>
                     </Row>
+                </Col>
+                <Col span={8}>
+                    <Row align='middle' justify='center'>
+                    <Space style={{height:50}}>
+                            <Button type='text' icon={<AppstoreOutlined />}>My WorkSpace <DownOutlined />
+                            </Button>
+                        <Button icon={<UserAddOutlined />}>invate</Button>
+                    </Space>
+                    </Row>
+                </Col>
+                <Col span={8}>
+                    <Row align='middle' justify='end'>
+                        <Space style={{height:50}}>
+                            <Button shape='circle-outline' icon={<HeartOutlined />}></Button>
+                            <Button shape='circle-outline' icon={<ReloadOutlined />}></Button>
+                            <Button shape='circle-outline' icon={<SettingOutlined />}></Button>
+                            <Button shape='circle-outline' icon={<HeartOutlined />}></Button>
+                            
+                        </Space>
+                    </Row>
                     
                 </Col>
-                <Col span={8}></Col>
-                <Col span={8}></Col>
             </Row>
         </Header>
     )
